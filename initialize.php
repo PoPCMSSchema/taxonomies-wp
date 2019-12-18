9c41ebb5c14e9a21e19dc75dccb8656e6d0e1008
Leo <losoviz@gmail.com>
2019-12-18 13:32:56 +0800

    Added TypeAPI

--- initialize.php
+++ initialize.php
@@ -1,1 +1,2 @@
 <?php
+\PoP\TaxonomiesWP\Component::init();
---
+++ src/TypeAPIs/TaxonomyTypeAPI.php
@@ -0,0 +1,21 @@
+<?php
+namespace PoP\TaxonomiesWP\TypeAPIs;
+
+use WP_Taxonomy;
+use PoP\Taxonomies\TypeAPIs\TaxonomyTypeAPIInterface;
+/**
+ * Methods to interact with the Type, to be implemented by the underlying CMS
+ */
+class TaxonomyTypeAPI implements TaxonomyTypeAPIInterface
+{
+    /**
+     * Indicates if the passed object is of type Taxonomy
+     *
+     * @param [type] $object
+     * @return boolean
+     */
+    public function isInstanceOfTagType($object): bool
+    {
+        return ($object instanceof WP_Taxonomy) && $object->hierarchical == false;
+    }
+}
