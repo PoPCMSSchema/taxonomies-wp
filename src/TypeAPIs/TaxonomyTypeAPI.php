c61156fc2ba0a37fe2520b8ea501e1835bb4be2c
Leonardo Losoviz <leo@getpop.org>
2020-07-16 14:38:14 +0800

    Transferred code to Tags

--- src/TypeAPIs/TaxonomyTypeAPI.php
+++ src/TypeAPIs/TaxonomyTypeAPI.php
@@ -4,21 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\TaxonomiesWP\TypeAPIs;
 
-use WP_Taxonomy;
 use PoP\Taxonomies\TypeAPIs\TaxonomyTypeAPIInterface;
+
 /**
  * Methods to interact with the Type, to be implemented by the underlying CMS
  */
 class TaxonomyTypeAPI implements TaxonomyTypeAPIInterface
 {
-    /**
-     * Indicates if the passed object is of type Taxonomy
-     *
-     * @param [type] $object
-     * @return boolean
-     */
-    public function isInstanceOfTagType($object): bool
-    {
-        return ($object instanceof WP_Taxonomy) && $object->hierarchical == false;
-    }
 }
